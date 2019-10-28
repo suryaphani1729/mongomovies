0c4349e07a8016c21c8453989928e81419c2bc6e
surya <surya.phani1729@gmail.com>
2019-10-28 15:40:48 +0530

    body-parser added

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const connectDb = require('./database');
+var bodyParser = require('body-parser');
 var cors = require('cors');
 
 
 const app = express();
 
 app.use(express.json({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 
